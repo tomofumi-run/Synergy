ab38a55a34f22e61aa3e31085e227803843da307
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-06-02 10:50:25 +0000

    [Add]論理削除

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_customer, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,10 +19,22 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def reject_customer
+    @user = User.find_by(email: params[:user][:email].downcase)
+    # ログイン時に入力されたemailが存在するか探す。
+    if @customer
+      if (@customer.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+    # 入力されたパスワードが正しいこと 且つ　active_for_authentication?メソッドがfalseであるかどうか。
+        redirect_to new_customer_session_path,alert:"このアカウントは退会済みです。"
+      end
+    else
+      flash[:alert] = "必須項目を入力してください"
+    end
+  end
 end
