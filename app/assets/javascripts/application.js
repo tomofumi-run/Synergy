7d1d2ccd24d2c66492b123c7ed1f2343b0d55a08
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-06-08 09:40:45 +0000

    [Fix]footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,3 +47,9 @@ document.addEventListener("turbolinks:load",function () {
     hidePrevious : false
   });
 });
+
+//logoの表示
+$(window).on('turbolinks:load',function(){
+  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
