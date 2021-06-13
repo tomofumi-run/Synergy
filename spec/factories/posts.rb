5826b26007f5e826b2f449321bb63ebdb8e451ae
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-06-13 00:02:30 +0000

    [Fix]RSpec[2]

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
  factory :post do
-    genre_id { 1 }
-    attach_file { "post[post_image]" } "app/assets/images/abe.jpg"
     title { Faker::Lorem.characters(number: 10) }
     content { Faker::Lorem.characters(number: 30)}
+    post_image_id{ Faker::Lorem.characters(number:10) } ##画像を入れるには？
+    genre
+    user
  end
 end
