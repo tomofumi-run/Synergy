62fb0d3db609fac9e4cf4353fcacf031b062f7dd
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-06-20 07:25:42 +0000

    [Add]jscroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 
 //= require jquery3
-//= require jquery
+//= require jquery.jscroll.min.js
 //= require popper
 //= require bootstrap
 //= require summernote/summernote-bs4.min
@@ -24,31 +24,30 @@
 
 document.addEventListener("turbolinks:load",function () {
   $(".theTarget").skippr({
-    // スライドショーの変化 ("fade" or "slide")
     transition : 'slide',
-    // 変化に係る時間(ミリ秒)
     speed : 2000,
-    // easingの種類
     easing : 'easeOutQuart',
-    // ナビゲーションの形("block" or "bubble")
     navType : 'block',
-    // 子要素の種類('div' or 'img')
     childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
     arrows : true,
-    // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
-    // 自動再生時のスライド切替間隔(ミリ秒)
     autoPlayDuration : 5000,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : true,
-    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
   });
 });
 
 //logoの表示
 $(window).on('turbolinks:load',function(){
-  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-  $("#splash_logo").delay(1000).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  $("#splash").delay(1500).fadeOut('slow');
+  $("#splash_logo").delay(1000).fadeOut('slow');
+});
+
+
+
+$(document).on('turbolinks:load', function() {
+  $('.jscroll').jscroll({
+    nextSelector: 'a[rel=next].page-link',
+    contentSelector: '.jscroll',
+  });
 });
