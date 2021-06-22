9397fc50b294f9bda87bdcd288b52b89f50cc97c
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-06-22 04:07:19 +0000

    [Fix]RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,4 +96,16 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+  ENV["RAILS_ENV"] ||= 'test'
+    require File.expand_path("../../config/environment", __FILE__)
+    require 'rspec/rails'
+    require 'support/capybara.rb'
+    
+    Dir[Rails.root.join("spec/support/*.rb")].each { |f| require f }
+    ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+    
+    RSpec.configure do |config|
+      config.include Rails.application.routes.url_helpers
+      config.include Capybara::DSL
+    end
 end
