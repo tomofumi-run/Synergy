f124cee35568395e7ef5f18d97dc9de5e4c25cb8
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-07-03 15:34:02 +0000

    [Fix]chart

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -3,7 +3,6 @@ class Admin::ChartsController < ApplicationController
  
  def post_chart
     posts = Post.all
-    @today_post = posts.where(created_at: Date.today.all_day).count
     @yesterday_post = posts.chart_data(1)
     @two_days_ago_post = posts.chart_data(2)
     @three_days_ago_post = posts.chart_data(3)
